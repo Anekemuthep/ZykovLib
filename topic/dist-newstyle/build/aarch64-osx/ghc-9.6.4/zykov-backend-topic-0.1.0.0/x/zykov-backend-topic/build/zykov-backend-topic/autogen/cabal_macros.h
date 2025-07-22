6a2520f803d8730e96bd6173281f20ff08796dbe
alfonsobustamante <athal.zigma@gmail.com>
2025-07-22 12:45:16 -0400

    Combo box version of topic selection implemented as index-topic-combox (html)

--- topic/dist-newstyle/build/aarch64-osx/ghc-9.6.4/zykov-backend-topic-0.1.0.0/x/zykov-backend-topic/build/zykov-backend-topic/autogen/cabal_macros.h
+++ topic/dist-newstyle/build/aarch64-osx/ghc-9.6.4/zykov-backend-topic-0.1.0.0/x/zykov-backend-topic/build/zykov-backend-topic/autogen/cabal_macros.h
@@ -70,6 +70,16 @@
   (major1) == 1 && (major2) <  3 || \
   (major1) == 1 && (major2) == 3 && (minor) <= 8)
 #endif /* MIN_VERSION_directory */
+/* package filepath-1.4.200.1 */
+#ifndef VERSION_filepath
+#define VERSION_filepath "1.4.200.1"
+#endif /* VERSION_filepath */
+#ifndef MIN_VERSION_filepath
+#define MIN_VERSION_filepath(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  4 || \
+  (major1) == 1 && (major2) == 4 && (minor) <= 200)
+#endif /* MIN_VERSION_filepath */
 /* package process-1.6.17.0 */
 #ifndef VERSION_process
 #define VERSION_process "1.6.17.0"
